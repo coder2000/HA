672d4610b1aa0c69274728583310556b4b778416
Dieter Lunn <coder2000@gmail.com>
2018-09-16 17:05:14 -0500

    Finish Ep 1

---
+++ Assets/Models/Tile.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile {
+    enum TileType { Empty, Floor };
+
+    TileType type = TileType.Empty;
+
+    LooseObject looseObject;
+    InstalledObject installedObject;
+
+    World world;
+    int x, y;
+
+    public Tile(World world, int x, int y)
+    {
+        this.world = world;
+        this.x = x;
+        this.y = y;
+    }
+}
---
+++ Assets/Models/World.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World {
+    Tile[,] tiles;
+    int width;
+    int height;
+
+    public World(int width = 100, int height = 100)
+    {
+        this.width = width;
+        this.height = height;
+
+        tiles = new Tile[width, height];
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                tiles[x, y] = new Tile(this, x, y);
+            }
+        }
+    }
+
+    public Tile GetTileAt(int x, int y)
+    {
+        return tiles[x, y];
+    }
+}
